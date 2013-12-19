cf79c5e400e9145638524abf8542512c27652970
Sven Woop <sven.woop@intel.com>
2013-12-19 08:22:37 +0100

    updates to benchmark.py

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -75,12 +75,12 @@ compilers_unix = ['ICC']
 compilers      = []
 
 #builds = ['Debug']
-#builds = ['Release']
+builds = ['Release']
 #builds = ['Release', 'Debug']
 #builds = ['Release']
 #builds = ['ReleaseAVX']
 #builds = ['ReleaseAVX2']
-builds = ['ReleaseAVX2', 'ReleaseAVX', 'Release']
+#builds = ['ReleaseAVX2', 'ReleaseAVX', 'Release']
 #builds = ['ReleaseAVX2', 'ReleaseAVX', 'Release', 'Debug']
 
 #platforms_win  = ['win32']
@@ -90,35 +90,45 @@ platforms_unix = ['x64']
 platforms      = []
 
 devices = [ 'singleray', 'ispc' ]
-scenes = ['conference']
-#scenes = ['conference', 'crown', 'headlight', 'nightgown', 'powerplant']
+#scenes = ['conference']
+scenes = ['conference', 'crown', 'headlight', 'nightgown', 'powerplant']
 #scenes = ['conference', 'courtyard', 'crown', 'e87', 'e89', 'e89_engine', 'headlight', 'loftcube', 'nightgown', 'powerplant', 'stanford', 'xyz_dragon']
 
 #isas_win  = ['sse41']
 #isas_win = ['sse3', 'sse41']
-isas_win = ['sse3', 'sse41', 'avx']
+#isas_win = ['sse3', 'sse41', 'avx']
 #isas_win = ['sse3', 'sse41', 'avx', 'avx2']
+isas4_win = ['sse3', 'sse41']
+isas8_win = ['avx']
+#isas8_win = ['avx', 'avx2']
+isas_win  = isas4_win + isas8_win
 
-isas_unix = ['sse3', 'sse41', 'avx']
+#isas4_unix = []
+isas4_unix = ['sse3', 'sse41']
+isas8_unix = ['avx']
+#isas8_unix = ['avx', 'avx2']
+isas_unix  = isas4_unix + isas8_unix
 #isas_unix = ['sse3', 'sse41', 'avx', 'avx2']
-isas      = []
-
-isas_allowed_ispc = {}
-isas_allowed_ispc['Debug'] = ['sse2', 'sse3', 'sse41']
-isas_allowed_ispc['Release'] = ['sse2', 'sse3', 'sse41']
-isas_allowed_ispc['ReleaseAVX'] = ['avx']
-isas_allowed_ispc['ReleaseAVX2'] = ['avx', 'avx2']
+isas4 = []
+isas8 = []
+isas  = []
 
 modelDir  = ''
 testDir = ''
-embreeDirLinux = '~/projects/embree.intel.git'
-embreeDirWindows = os.environ['EMBREE_INSTALL_DIR']
+embreeDirLinux = '~/projects/embree.git'
+try:
+  embreeDirWindows = os.environ['EMBREE_INSTALL_DIR']
+except KeyError:
+  embreeDirWindows = ""
 
 ########################## compiling ##########################
 
-def compile(OS,compiler,platform,isas,build):
+def compile(OS,compiler,platform,isas,build,simd):
   if OS == 'windows':
   
+#    if simd == 8: cfg = cfg + 'AVX2'
+    if simd == 8: cfg = cfg + 'AVX'
+
     cfg = '/p:Configuration=' + build + ';'
     cfg += 'Platform=' + platform + ';'
     cfg += 'PlatformToolset=';
@@ -172,7 +182,7 @@ def compile(OS,compiler,platform,isas,build):
     command += ' -D RTCORE_FIX_RAYS=OFF'
 
     if (compiler == 'ICC'):
-      command += ' -D TARGET_SSE41=ON'
+      command += ' -D TARGET_SSE41=ON';
       command += ' -D TARGET_AVX=ON'
       command += ' -D TARGET_AVX2=ON'
     else:
@@ -193,8 +203,12 @@ def compile(OS,compiler,platform,isas,build):
     command += ' -D TARGET_SSE2=ON'
     if (compiler == 'ICC'):
       command += ' -D TARGET_SSE41=ON'
-      command += ' -D TARGET_AVX=ON'
-      command += ' -D TARGET_AVX2=ON'
+      if (simd == 8):
+        command += ' -D TARGET_AVX=ON'
+        command += ' -D TARGET_AVX2=ON'
+      else:
+        command += ' -D TARGET_AVX=OFF'
+        command += ' -D TARGET_AVX2=OFF'
     else:
       command += ' -D TARGET_SSE41=OFF'
       command += ' -D TARGET_AVX=OFF'
@@ -218,8 +232,8 @@ def compileLoop(OS):
       for platform in platforms:
         for build in builds:
           print(OS + ' ' + compiler + ' ' + platform + ' ' + build)
-          compile(OS,compiler,platform,isas,build)
-
+          compile(OS,compiler,platform,isas,build,4)
+          compile(OS,compiler,platform,isas,build,8)
 
 ########################## rendering ##########################
 
@@ -246,14 +260,20 @@ def renderLoop(OS):
     for compiler in compilers:
       for platform in platforms:
         for build in builds:
-          compile(OS,compiler,platform,isas,build)
-          for scene in scenes:
-            for device in devices:
-              for isa in isas:
-                 if device <> 'ispc' or (isa in isas_allowed_ispc[build]):
-                   print(compiler + ' ' + platform + ' ' + build + ' ' + scene + ' ' + device + ' ' + isa)
-                   render(OS, scene, compiler, platform, build, device, isa)
-
+          if isas4 <> []:
+            compile(OS,compiler,platform,isas,build,4)
+            for scene in scenes:
+              for device in devices:
+                for isa in isas4:
+                  print(compiler + ' ' + platform + ' ' + build + ' ' + scene + ' ' + device + ' ' + isa)
+                  render(OS, scene, compiler, platform, build, device, isa)
+          if isas8 <> []:
+            compile(OS,compiler,platform,isas,build,8)
+            for scene in scenes:
+              for device in devices:
+                for isa in isas8:
+                  print(compiler + ' ' + platform + ' ' + build + ' ' + scene + ' ' + device + ' ' + isa)
+                  render(OS, scene, compiler, platform, build, device, isa)
 
 ########################## data extraction ##########################
 
@@ -461,11 +481,16 @@ if OS == 'windows':
   compilers = compilers_win
   platforms = platforms_win
   isas = isas_win
+  isas4 = isas4_win
+  isas8 = isas8_win
+
 else:
   dash = '/'
   compilers = compilers_unix
   platforms = platforms_unix
   isas = isas_unix
+  isas4 = isas4_unix
+  isas8 = isas8_unix
 
 if mode == 'compile':
   compileLoop(OS)
