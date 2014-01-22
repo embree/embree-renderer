61891b6ecdd247fa35a1cc5a1ae33048e6e66fe5
Sven Woop <sven.woop@intel.com>
2014-01-22 12:56:24 +0100

    disabled --math-lib=fast in ISPC, causes pixel errors with crown

--- common/cmake/ispc.cmake
+++ common/cmake/ispc.cmake
@@ -95,7 +95,7 @@ MACRO (ispc_compile targets)
       --target=${TARGETS}
       --wno-perf
       --opt=fast-math
-      --math-lib=fast
+#      --math-lib=fast
       ${ISPC_TARGET_ALIGNED_MEMORY}
 #      --opt=force-aligned-memory
       -h ${outdirh}/${fname}_ispc.h
