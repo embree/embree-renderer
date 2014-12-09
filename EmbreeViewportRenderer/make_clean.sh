e5b750f7d934c71ee9b5bf958f4deedcd651ae6c
Charles W Congdon <charles.w.congdon@intel.com>
2014-12-09 10:27:06 -0800

    Improve cleaning of plugin on linux

--- EmbreeViewportRenderer/make_clean.sh
+++ EmbreeViewportRenderer/make_clean.sh
@@ -1,3 +1,3 @@
-rm -f ../build/EmbreeViewportRenderer.o 
-rm -f ../build/EmbreeViewportRendererPluginSX.o ../build/EmbreeViewportRendererPluginSXP.o 
-rm -f ../build/EmbreeViewportRendererPluginIX.o ../build/EmbreeViewportRendererPluginIXP.o ../build/EmbreeViewportRendererPluginIH.o
+rm -f ../build/EmbreeViewportRenderer.*o 
+rm -f ../build/EmbreeViewportRendererPluginSX.*o ../build/EmbreeViewportRendererPluginSXP.*o 
+rm -f ../build/EmbreeViewportRendererPluginIX.*o ../build/EmbreeViewportRendererPluginIXP.*o ../build/EmbreeViewportRendererPluginIH.*o
